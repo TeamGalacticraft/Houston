8328d3d588e6817cfb3fa09277a0ab292643883b
Pyrrha Wills <pyrrha.social@gmail.com>
2022-12-10 16:17:44 +1000

    feat: seed db with default admin user

---
+++ migrations/20221210061331_seed-users.down.sql
@@ -0,0 +1,3 @@
+delete
+from users
+where id = '7662d989-17e0-4797-bb21-23745ea59664';
---
+++ migrations/20221210061331_seed-users.up.sql
@@ -0,0 +1,5 @@
+insert into users (id, username, avatar_url, roles)
+values ('7662d989-17e0-4797-bb21-23745ea59664',
+        'JustPyrrha',
+        'https://visage.surgeplay.com/face/512/7662d98917e04797bb2123745ea59664',
+        array ['admin']);
